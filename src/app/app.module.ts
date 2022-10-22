697664ca248ba0f12a080f01f397feb94fc2aa04
katycassiday <114358777+katycassiday@users.noreply.github.com>
2022-10-22 09:37:37 -0700

    forms
    contact form started-styles not correct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ContactComponent } from './contact/contact.component';
+import { ContactUsComponent } from './contact/contact-form/contact-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -0,0 +1,65 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  AbstractControl,
+  Validators,
+  MinLengthValidator,
+} from "@angular/forms";
+
+@Component({
+  selector: "app-contact-form",
+  templateUrl: "./contact-form.component.html",
+  styleUrls: ["./contact-form.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactUsComponent {
+public contactForm: FormGroup ;
+  
+  public get email(): AbstractControl | null {
+    return this.contactForm.get("email");
+  }
+
+  public get reason(): AbstractControl | null {
+    return this.contactForm.get("reason");
+  }
+
+  public get description(): AbstractControl | null {
+    return this.contactForm.get("description");
+  }
+
+  @Output() formSubmit = new EventEmitter<{
+    email: string;
+    reason: string;
+    description: string;
+  }>();
+
+  constructor() {
+    this.contactForm = this._buildForm();
+  };
+ 
+  public onSubmit(): void {
+    this.contactForm.markAllAsTouched();
+    if (this.contactForm.valid) {
+      console.log(
+        "VALID! ContactUs form submit", 
+      this.contactForm.value
+      );
+      this.formSubmit.emit(this.contactForm.value);
+    }
+  }
+
+  private _buildForm(): FormGroup {
+    return new FormGroup(
+      {
+      email: new FormControl("", [Validators.required, Validators.email]),
+      reason: new FormControl("", [Validators.required]),
+      descripion: new FormControl("", [Validators.required]),
+    });
+  }
+}
